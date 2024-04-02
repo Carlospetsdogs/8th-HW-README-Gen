c31ea4d5355d81d1e2da07c47379ab934edf3c92
Carlos Ruiz <carlosruiz9221@gmail.com>
2024-04-02 11:18:17 -0400

    added all user questions to array

--- index.js
+++ index.js
@@ -13,8 +13,48 @@ const questions = [
     message:'Input your GitHub username'
 },
 {
-    
-}
+    type: 'input',
+    name: 'email',
+    message: 'Input your preffered email address'
+},
+{
+    type:'input',
+    name:'project title',
+    message:'Input is your projects title',
+},
+{
+    type:'input',
+    name:'description',
+    message:'Briefly describe your project',
+},
+{
+    type:'list',
+    name:'license',
+    messaage:'Choose what license your project will have:',
+    options:['MIT','GNU GPLv3','Apache 2.0','BDS 2','BDS 3','Mozilla','Creative','None'],
+},
+{
+    type:'input',
+    name:'installation',
+    message:'What command is needed to install dependancies?',
+    default:'npm i',
+},
+{
+    type:'input',
+    name:'test',
+    messaage:'Command to run tests',
+    default:'nmp test',
+},
+{
+    type:'input',
+    name:'usage',
+    message:'Relevant info for usage of your repo for the user:'
+},
+{
+    type:'input',
+    name:'contritbute info',
+    message:'Relevant info for the user about contributing to repo:'
+},
 ];
 
 // TODO: Create a function to write README file
