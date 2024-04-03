4c202f29dc9a9ef57750d0674d8bb9176167da54
Carlos Ruiz <carlosruiz9221@gmail.com>
2024-04-03 10:00:58 -0400

    added functions to render chosen license

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,34 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// function to render chosen license
+function getLicenseBadge(license) {
+  switch (license) {
+    case 'MIT':
+      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+    case 'GNU GPLv3':
+      return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+    case 'Apache 2.0':
+      return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+    case 'BDS 2':
+      return '[![License](https://img.shields.io/badge/License-BDS%202-blue.svg)](https://opensource.org/licenses/BSD-2-Clause)';
+    case 'BDS 3':
+      return '[![License](https://img.shields.io/badge/License-BDS%203-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)';
+    case 'Mozilla':
+      return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
+    case 'Creative':
+      return '[![License: CC BY 4.0](https://img.shields.io/badge/License-CC%20BY%204.0-lightgrey.svg)](https://creativecommons.org/licenses/by/4.0/)';
+    case 'None':
+      return '';
+    default:
+      return '';
+  }
+}
+
+// function to get the license badge based on the license name
+const licenseBadge = getLicenseBadge('MIT');
+console.log(licenseBadge);
+
+
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
